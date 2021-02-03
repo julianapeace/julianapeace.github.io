3f9d8f6b881faac00651f26018b606065a8da7f9
jmei <>
2021-02-03 15:59:30 -0500

    updated my bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -2,4 +2,6 @@ echo 'alias goproxy="export http_proxy=http://192.168.1.11:8080"' >> ~/.bashrc
 echo  "alias disproxy='unset http_proxy'" >> ~/.bashrc
 echo "alias ..='cd ..'" >> ~/.bashrc
 echo "alias ...='cd ../..'" >> ~/.bashrc
+echo "alias pm2-start='pm2 start --name=notification npm -- run dev'" >> ~/.bashrc
+echo "alias yeet='docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'" >> ~/.bashrc
 source ~/.bashrc
